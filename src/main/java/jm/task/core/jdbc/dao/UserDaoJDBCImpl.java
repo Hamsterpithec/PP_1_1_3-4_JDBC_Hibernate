66d2f070015372a30d196654a47009d01194bcf6
Alex <locher91@mail.ru>
2024-03-07 13:30:43 +0300

    first solution

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,107 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+    private final Connection connection = Util.getConnection();
+
+    public UserDaoJDBCImpl() {
     }
 
-    public void createUsersTable() {
+    public void createUsersTable() throws SQLException {
+        String sql = "CREATE TABLE IF NOT EXISTS usertable (" +
+                "  id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT ," +
+                "  name VARCHAR(100) NOT NULL," +
+                "  lastname VARCHAR(110) NOT Null," +
+                "  age TINYINT NOT NULL)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     public void dropUsersTable() {
+        String sql= "DROP TABLE IF EXISTS usertable";
+        try (PreparedStatement preparedStatement= connection.prepareStatement(sql)){
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String sql= "INSERT INTO usertable (name, lastname, age)" + "VALUES (?,?,?)";
+        try (PreparedStatement preparedStatement=connection.prepareStatement(sql)){
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,lastName);
+            preparedStatement.setByte(3,age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
     public void removeUserById(long id) {
+        String sql ="DELETE FROM usertable WHERE id = ?";
+        try (PreparedStatement preparedStatement= connection.prepareStatement(sql)){
+            preparedStatement.setLong(1, id);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> usersList= new ArrayList<>();
+        String sql= "SELECT * FROM usertable";
+        PreparedStatement preparedStatement=null;
+
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet= preparedStatement.executeQuery();
+            while (resultSet.next()){
+                User user=new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastname"));
+                user.setAge(resultSet.getByte("age"));
+
+                usersList.add(user);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return usersList;
     }
 
     public void cleanUsersTable() {
-
+        String sql = "DROP TABLE IF EXISTS usertable";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String USERNAME = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/pp_1_1_1_4db";
+    private static final String PASSWORD = "Admin_1337";
+    public static Connection getConnection() {
+
+        Connection connection = null;
+            try {
+
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            } catch (SQLException e) {
+                System.out.println("Connection failed...");
+
+            }
+        return connection;
+    }
 }
