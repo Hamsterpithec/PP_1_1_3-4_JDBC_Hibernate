267775ace4a4123dca5501871b3361f0d4b68040
Alex <locher91@mail.ru>
2024-03-11 17:29:17 +0300

    11_03 solution

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -9,10 +10,12 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
+
     private final UserDao userDao= new UserDaoJDBCImpl();
 
     public void createUsersTable() throws SQLException {
         userDao.createUsersTable();
+
 
     }
 
@@ -22,7 +25,7 @@ public class UserServiceImpl implements UserService {
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
         userDao.saveUser(name, lastName, age);
-        System.out.println("User с именем — " + name + " добавлен в базу данных");
+
 
     }
 
